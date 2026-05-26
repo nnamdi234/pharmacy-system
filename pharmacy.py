206e18a161ae3dbe3dfeac5774b1d8fa7261bf6d
Nnamdi <nnamdiau@gmail.com>
2026-05-26 13:33:31 +0100

    added pharmacysystem'

---
+++ pharmacy.py
@@ -0,0 +1,29 @@
+def show_menu():
+    print("")
+    print(" Pharmacy system ")
+    print(" 1. Add / Restock a drug ")
+    print(" 2. Search for a drug")
+    print(" 3. Dipense drug to customer")
+    print(" 4. Restock a drug ")
+    print(" 5. Remove a drug ")
+    print(" 6. List all drugs ")
+    print(" 7. Browse by category ")
+    print(" 8. View sales log ")
+    print(" 0. Exit ")
+
+
+
+
+
+
+
+
+def main():
+    print("")
+    print(" Welcome to the Pharmacy System")
+    # print("" + str(len(pharmacy)) + " drugs loaded. ")
+
+    show_menu()
+
+
+main()
